e4db5b5b0cad8aa349d770fbabb846f72cb0a4c5
OvsienkoValeriya <lerchiknet@gmail.com>
2018-10-24 19:20:30 +0300

    ...

--- bot.py
+++ bot.py
@@ -5,12 +5,12 @@ bot = telebot.TeleBot(TOKEN)
 
 
 @bot.message_handler(commands=['start'])
-def startCommand(message):
+def send_welcome(message):
     bot.reply_to(message, "Привет, хочешь чаю?")
 
 
 @bot.message_handler(func=lambda m: True)
-def textMessage(message):
+def send_message(message):
     bot.reply_to(message, "Отлично:" + message.text)
 
 bot.polling()
