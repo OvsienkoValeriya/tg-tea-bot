98c2247ebec7f66eb4fc7f1e69baac79b0d3288b
OvsienkoValeriya <lerchiknet@gmail.com>
2018-10-24 19:41:32 +0300

    what tea

--- bot.py
+++ bot.py
@@ -6,11 +6,11 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    bot.reply_to(message, "Привет, хочешь чаю?")
+    bot.send_message(message, "Привет, какого хочешь чаю?")
 
 
 @bot.message_handler(func=lambda m: True)
 def send_message(message):
-    bot.reply_to(message, "Отлично:" + message.text)
+    bot.send_message(message, "Отлично:" + message.text)
 
 bot.polling()
