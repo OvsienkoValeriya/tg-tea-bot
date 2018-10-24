0dce8e1384f3fe0aee0dd6529c3195542a9e07ee
OvsienkoValeriya <lerchiknet@gmail.com>
2018-10-24 19:16:14 +0300

    move to telebot

--- bot.py
+++ bot.py
@@ -1,24 +1,16 @@
-import requests
+import telebot
+
+TOKEN = "789845045:AAF4GvK-9DYhedra0Vc47ZPKku93KjNmUAQ"
+bot = telebot.TeleBot(TOKEN)
 
 
-url="https://api.telegram.org/bot789845045:AAF4GvK-9DYhedra0Vc47ZPKku93KjNmUAQ/"
+@bot.message_handler(commands=['start'])
+def startCommand(message):
+    bot.reply_to(message, "Привет, хочешь чаю?")
 
-from telegram.ext import Updater,
-updater = Updater(token="789845045:AAF4GvK-9DYhedra0Vc47ZPKku93KjNmUAQ")
-dispatcher = updater.dispatcher
 
-def get_updates_json(request):
-    response = requests.get(request + 'getUpdates')
-    return response.json()
+@bot.message_handler(func=lambda m: True)
+def textMessage(message):
+    bot.reply_to(message, "Отлично:" + message.text)
 
-def last_update(data):
-    results = data['result']
-    total_updates = len(results) - 1
-    return results[total_updates]
-
-def startCommand(bot,update):
-    bot.send_message(chat_id=update.message.chat_id, text="Привет, хочешь чаю?")
-
-def textMessage(bot, update):
-    response = "Отлично:" + update.message.text
-    
+bot.polling()
