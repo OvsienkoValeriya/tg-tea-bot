5fab1641faedf9978f529808b263d408c4478de7
OvsienkoValeriya <lerchiknet@gmail.com>
2018-10-24 21:11:31 +0300

    menu

---
+++ Usecases/__init__.py
@@ -0,0 +1,27 @@
+import telebot
+from telebot import types
+
+TOKEN = "789845045:AAF4GvK-9DYhedra0Vc47ZPKku93KjNmUAQ"
+bot = telebot.TeleBot(TOKEN)
+
+
+def tea_menu_predicate(message):
+    text = message.text.lowercase()
+    return text == "какой бывает чай?" or \
+        text == "какой чай бывает?" or \
+        text == "не знаю"
+
+
+def tea_menu_make_markup():
+    markup = types.ReplyKeyboardMarkup(row_width=3)
+    itembtn_green = types.KeyboardButton ("Зеленый")
+    itembtn_white = types.KeyboardButton ("Белый")
+    itembtn_hell_turquois = types.KeyboardButton ("Светлый Улун")
+    itembtn_dark_turquois = types.KeyboardButton ("Темный Улун")
+    itembtn_red = types.KeyboardButton ("Красный")
+    itembtn_shen = types.KeyboardButton ("Шен Пуэр")
+    itembtn_shu = types.KeyboardButton ("Шу Пуэр")
+    itembtn_black = types.KeyboardButton ("Чёрный Хэй Ча")
+    itembtn_help = types.KeyboardButton ("Помогите, я ничего не понимаю")
+    markup.add (itembtn_green, itembtn_white, itembtn_hell_turquois, itembtn_dark_turquois, itembtn_red, itembtn_shen, itembtn_shu, itembtn_black, itembtn_help)
+    return markup
--- bot.py
+++ bot.py
@@ -1,4 +1,6 @@
 import telebot
+from telebot import types
+from Usecases import *
 
 TOKEN = "789845045:AAF4GvK-9DYhedra0Vc47ZPKku93KjNmUAQ"
 bot = telebot.TeleBot(TOKEN)
@@ -9,12 +11,21 @@ def send_welcome(message):
     chat_id= message.chat.id
     bot.send_message(chat_id, "Привет, какого хочешь чаю?")
 
+@bot.message_handler(commands=['tea_menu'], func=tea_menu_predicate)
+def send_tea_menu(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Выбери вариант", reply_markup=tea_menu_make_markup())
+
+
+@bot.message_handler(regexp="(Я|я) (С|с)аша")
+def send_easter_egg(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Тогда пей вискарь и не выебывайся")
+
 
 @bot.message_handler(func=lambda m: True)
 def send_message(message):
     chat_id= message.chat.id
     bot.send_message(chat_id, "Отлично:" + message.text)
-    if message.chat.id=="Я Саша":
-        bot.send_message(chat_id, "Рекомендую выпить виски")
 
 bot.polling()
