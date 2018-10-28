361c573b28bcb2572e1322ebb3201b3442d12b2d
OvsienkoValeriya <lerchiknet@gmail.com>
2018-10-28 15:05:47 +0300

    total_new

--- Usecases/__init__.py
+++ Usecases/__init__.py
@@ -1,15 +1,15 @@
 import telebot
 from telebot import types
 
-GREEN = "зеленый"
+GREEN = "зелёный"
 WHITE = "белый"
 HELL_TURQUOIS = "светлый улун"
-DARK_TURQUOIS = "темный улун"
+DARK_TURQUOIS = "тёмный улун"
 RED = "красный"
 SHEN = "шен пуэр"
 SHU = "шу пуэр"
 BLACK = "чёрный хэй ча"
-HELP = "что?"
+YELLOW = "жёлтый"
 
 TEA_DESCRIPTION = {
     GREEN: "Зеленый чай самый бодрящий",
@@ -20,35 +20,123 @@ TEA_DESCRIPTION = {
     SHEN: "Чем старше шен - тем он вкусней",
     SHU: "Шу Пуэр любит Лера",
     BLACK: "На черном чае можно увидеть плесень",
-    HELP: "Сейчас я все объясню"
+    YELLOW: "Желтый - очень дорогой"
+}
+TEA_CHOOSE_CATEGORY = "по категории"
+TEA_CHOOSE_FLAVOUR = "по вкусу"
+TEA_CHOOSE_CONDITION = "по состоянию"
+TEA_CHOOSE_RELATED = "по похожему"
+
+VEGAN = "растительный",
+SWEET = "сладко-кондитерский",
+BERRY = "кисло-ягодный",
+FRUIT = "фруктовый",
+FLOWER = "цветочный",
+VEGETABLE = "овощной",
+NUTS = "ореховый",
+SPICY = "пряный",
+TREE = "древесный",
+
+TEA_FLAVOUR = {
+    VEGAN: "Пей зелёнку",
+    SWEET: "Любимый чай мамы - женьшень улун",
+    BERRY: "Шен",
+    FRUIT: "Почувствуйте сирень в Те Гуан Ине",
+    FLOWER: "Снова зеленка",
+    VEGETABLE: "Шены-ваша судьба",
+    NUTS: "Пейте шу и будьте счастливы",
+    SPICY: "Темные улуны (их пьет Миша)",
+    TREE: "Шу - ваш выбор"
 }
 
+ALIVE = "бодрящий"
+QUIET = "успокаивающий"
 
-def tea_menu_predicate(message):
-    text = message.text.lower()
-    return text == "какой бывает чай?" or \
-        text == "какой чай бывает?" or \
-        text == "не знаю"
+TEA_CONDITION = {
+    ALIVE: "пей шу и все остальные чаи",
+    QUIET: "габа - твой выбор"
+}
+def make_word_in_list_predicate(lst):
+    return lambda message: message.text.lower() in lst
 
 
-def tea_menu_make_markup():
-    markup = types.ReplyKeyboardMarkup(row_width=3)
-    itembtn_green = types.KeyboardButton (GREEN.capitalize())
-    itembtn_white = types.KeyboardButton (WHITE.capitalize())
-    itembtn_hell_turquois = types.KeyboardButton (HELL_TURQUOIS.capitalize())
-    itembtn_dark_turquois = types.KeyboardButton (DARK_TURQUOIS.capitalize())
-    itembtn_red = types.KeyboardButton (RED.capitalize())
-    itembtn_shen = types.KeyboardButton (SHEN.capitalize())
-    itembtn_shu = types.KeyboardButton (SHU.capitalize())
-    itembtn_black = types.KeyboardButton (BLACK.capitalize())
-    itembtn_help = types.KeyboardButton (HELP.capitalize())
-    markup.add (itembtn_green, itembtn_white, itembtn_hell_turquois, itembtn_dark_turquois, itembtn_red, itembtn_shen, itembtn_shu, itembtn_black, itembtn_help)
+# делаем из списка слов кнопки и добавляем их в markup
+def fill_markup_with_words(markup, lst):
+    for word in lst:
+        markup.add(types.KeyboardButton(word))
     return markup
 
-def tea_menu_choose_predicate(message):
-    text = message.text.lower()
-    return text in [GREEN, WHITE, HELL_TURQUOIS, DARK_TURQUOIS, RED, SHEN, SHU, BLACK, HELP]
 
-def handle_tea_type(message:str):
+#
+# меню по категориям подбора
+#
+
+def tea_choose_make_markup():
+    markup = types.ReplyKeyboardMarkup(row_width=2)
+    tea_choose = [TEA_CHOOSE_CATEGORY, TEA_CHOOSE_FLAVOUR, TEA_CHOOSE_CONDITION, TEA_CHOOSE_RELATED]
+    return fill_markup_with_words(markup, tea_choose)
+
+
+tea_menu_predicate = make_word_in_list_predicate([TEA_CHOOSE_CATEGORY])
+
+
+# меню по типу чая
+def tea_menu_make_markup():
+    markup = types.ReplyKeyboardMarkup(row_width=3)
+    tea_words = [GREEN, WHITE, HELL_TURQUOIS, DARK_TURQUOIS, RED, SHEN, SHU, BLACK, YELLOW]
+    capitalized_tea_words = []
+    for tea_word in tea_words:
+        capitalized_tea_words.append(tea_word.capitalize())
+    return fill_markup_with_words(markup, capitalized_tea_words)
+
+
+tea_menu_choose_predicate = make_word_in_list_predicate(
+    [GREEN, WHITE, HELL_TURQUOIS, DARK_TURQUOIS, RED, SHEN, SHU, BLACK, YELLOW])
+
+
+# меню по типу чая. отображение описания
+def handle_tea_type(message: str):
     text = message.lower()
     return TEA_DESCRIPTION[text]
+
+
+# меню по вкусам чая
+def tea_flavour_make_markup():
+    markup = types.ReplyKeyboardMarkup(row_width=3)
+    tea_flavours = [VEGAN, SWEET, BERRY, FRUIT, FLOWER, VEGETABLE, NUTS, SPICY, TREE]
+    capitalized_tea_flavours = []
+    for tea_flavour in tea_flavours:
+        capitalized_tea_flavours.append(tea_flavour.capitalize())
+    return fill_markup_with_words(markup, capitalized_tea_flavours)
+
+
+tea_flavour_predicate = make_word_in_list_predicate([TEA_CHOOSE_FLAVOUR])
+
+tea_flavour_choose_predicate = make_word_in_list_predicate(
+    [VEGAN, SWEET, BERRY, FRUIT, FLOWER, VEGETABLE, NUTS, SPICY, TREE])
+
+
+# меню по вкусам чая. отображение описания
+def handle_tea_flavour(message: str):
+    text = message.lower()
+    return TEA_FLAVOUR[text]
+
+#меню по состоянию
+tea_condition_predicate = make_word_in_list_predicate([TEA_CHOOSE_CONDITION])
+
+def tea_condition_make_markup():
+    tea_conditions = [ALIVE, QUIET]
+    capitalized_tea_conditions = []
+    for tea_condition in tea_conditions:
+        capitalized_tea_conditions.append(tea_condition.capitalize())
+    return fill_markup_with_words(markup, capitalized_tea_conditions)
+
+tea_condition_choose_predicate = make_word_in_list_predicate([ALIVE, QUIET])
+
+#меню по состоянию. отображение описания
+def handle_tea_condition(message:str):
+    text = message.lower()
+    return TEA_CONDITION[text]
+
+#меню по стостоянию
+tea_related_predicate = make_word_in_list_predicate([TEA_CHOOSE_RELATED])
--- bot.py
+++ bot.py
@@ -8,32 +8,56 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def send_welcome(message):
-    chat_id= message.chat.id
-    bot.send_message(chat_id, "Привет, какого хочешь чаю?")
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Сегодня прекрасный день. Как вам подобрать чай?", reply_markup=tea_choose_make_markup())
 
-@bot.message_handler(commands=['tea_menu'])
+#по категории. показывает меню
+@bot.message_handler(commands=['by_category'])
 @bot.message_handler(func=tea_menu_predicate)
 def send_tea_menu(message):
     chat_id = message.chat.id
-    bot.send_message(chat_id, "Выбери вариант", reply_markup=tea_menu_make_markup() )
+    bot.send_message(chat_id, "Тогда выбери категорию", reply_markup=tea_menu_make_markup())
 
-
+#по категории. показывает текст после выбора
 @bot.message_handler(func=tea_menu_choose_predicate)
 def send_tea_menu_choose(message):
     chat_id = message.chat.id
-    bot.send_message(chat_id, text = handle_tea_type(message.text))
+    bot.send_message(chat_id, text=handle_tea_type(message.text))
+
+#по вкусу
+@bot.message_handler(commands = ['by_flavour'])
+@bot.message_handler(func=tea_flavour_predicate)
+def send_flavour_menu(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Тогда выбери категорию", reply_markup=tea_flavour_make_markup())
+
+#по вкусу. показывает текст после выбора
+@bot.message_handler(func=tea_flavour_choose_predicate)
+def send_tea_flavour_choose(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, text=handle_tea_flavour(message.text))
+
+#по состоянию
+@bot.message_handler(commands = ['by_condition'])
+@bot.message_handler(func=tea_condition_predicate)
+def send_predicate_menu(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Тогда выбери категорию", reply_markup=tea_condition_make_markup())
+
+#по похожему
+@bot.message.handler(commands = ['by_same'])
+@bot.message.hangler(func=tea_related_predicate)
+def send_sorry(message):
+    chat_id = message.chat.id
+    bot.send_message(chat_id, "Раздел находится в разработке")
 
 
-
+#пасхалка
 @bot.message_handler(regexp="(Я|я) (С|с)аша")
 def send_easter_egg(message):
     chat_id = message.chat.id
     bot.send_message(chat_id, "Тогда пей вискарь и не выебывайся")
 
 
-@bot.message_handler(func=lambda m: not tea_menu_predicate(m))
-def send_message(message):
-    chat_id= message.chat.id
-    bot.send_message(chat_id, "Отлично:" + message.text)
-
+# запускает бота
 bot.polling()
