ca23187b542dde95a30fc9a46f37e89f51d2510f
Alexandr Peshkov <42466181+AlexandrPeshkov@users.noreply.github.com>
2018-11-05 09:42:41 +0300

    Важный файл
    Важное описание

---
+++ cancer_egg.py
@@ -0,0 +1,15 @@
+from Usecases import *
+
+predicate = make_word_in_list_predicate(["Лера, мне грустно"])
+
+
+def handle():
+    return "Наверно, у тебя рак пятки, надо резать"
+
+
+def commands():
+    return ["CANCER_LADY666"]
+
+
+def markup():
+    return None
